3f168a009f5a268fe92f268693d5f67db8f61532
iClouds <48052351+WebScaffolder@users.noreply.github.com>
2022-11-04 17:51:50 +0000

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,4 +1,4 @@
 const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://luan-react.hibotheme.com' 
+? 'https://extra-ecom-final.vercel.app' 
 : 'http://localhost:3000';
 export default baseUrl;
