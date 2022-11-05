c1f972c3d1fa2c1c5e15485d2b2bc5f6c03f229a
iClouds <48052351+WebScaffolder@users.noreply.github.com>
2022-11-05 00:24:39 +0000

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-const baseUrl = 'https://obscure-beyond-65777.herokuapp.com';
-export default baseUrl;
-=======
 const baseUrl = 'https://extra-ecom.vercel.app';
 export default baseUrl;
->>>>>>> 9bf25876647eb81c0f2e69b4aa167f3f0f2d9ee9
+
