a7e245a44789f83a4635c3692036347d7e0d1e55
iClouds App Stall <inlaysoft@yahoo.com>
2022-11-05 13:21:51 +0000

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-const baseUrl = 'https://obscure-beyond-65777.herokuapp.com';
+const baseUrl = 'https://luan-react.hibotheme.com';
 export default baseUrl;
-=======
-const baseUrl = 'https://extra-ecom.vercel.app';
-export default baseUrl;
->>>>>>> 9bf25876647eb81c0f2e69b4aa167f3f0f2d9ee9
