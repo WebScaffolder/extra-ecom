7f4fe67f1b1106443a9df6bc736ce02e50003842
iClouds <48052351+WebScaffolder@users.noreply.github.com>
2022-11-06 22:45:29 +0000

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,2 +1,3 @@
-const baseUrl = 'https://luan-react.hibotheme.com';
+//const baseUrl = 'https://luan-react.hibotheme.com';
+const baseUrl = 'https://6368377715da5d2d84e2a404--graceful-chaja-892ca4.netlify.app';
 export default baseUrl;
