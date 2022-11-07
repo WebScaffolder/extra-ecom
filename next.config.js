61d0aa9494007f452c711fa2fb98741ce4ea2a55
iClouds <48052351+WebScaffolder@users.noreply.github.com>
2022-11-07 22:17:07 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,13 +9,13 @@ module.exports = {
     env: {
         JWT_SECRET: "djhfghbdsgrasklkajsdgf",
         // Update your cloudinary access credentials here
-        CLOUDINARY_URL: "...",
-		CLOUDINARY_NAME: "...",
-		UPLOAD_PRESET: "...",
+        CLOUDINARY_URL: "",
+		CLOUDINARY_NAME: "",
+		UPLOAD_PRESET: "",
         
         // Update your Stripe Secret Key & Publishable Key here
-        STRIPE_SECRET_KEY: "...",
-        STRIPE_PUBLISHABLE_KEY: "...",
+        STRIPE_SECRET_KEY: "",
+        STRIPE_PUBLISHABLE_KEY: "",
 	DATABASE_URL: "postgres://tbnjhebqixntzg:94891aae919f9e9869897897cf5171dae7a7e412c96e2ccb1936bd874c095405@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/d9ms4408n3hl30"
     },
     trailingSlash: false
