d6276c4b944bd9a4d20b7c55ae6a832aad0f9dd7
iClouds <48052351+WebScaffolder@users.noreply.github.com>
2022-11-07 21:42:05 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -15,7 +15,8 @@ module.exports = {
         
         // Update your Stripe Secret Key & Publishable Key here
         STRIPE_SECRET_KEY: "...",
-        STRIPE_PUBLISHABLE_KEY: "..."
+        STRIPE_PUBLISHABLE_KEY: "...",
+	DATABASE_URL: "postgres://tbnjhebqixntzg:94891aae919f9e9869897897cf5171dae7a7e412c96e2ccb1936bd874c095405@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/d9ms4408n3hl30"
     },
     trailingSlash: false
 }
