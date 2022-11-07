3e69cf6bccaa3fcc772688fc57e6bdba5c170339
iClouds <48052351+WebScaffolder@users.noreply.github.com>
2022-11-07 22:07:46 +0000

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
+const env = 'production';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
