ad8f0ccda93c81c033e42de3443ee171097640cc
iClouds <48052351+WebScaffolder@users.noreply.github.com>
2022-11-08 10:29:17 +0000

    Update index.js

--- models/index.js
+++ models/index.js
@@ -15,7 +15,7 @@ console.log("xxxx", config);
 let sequelize;
 if (config.use_env_variable) {
   //sequelize = new Sequelize(process.env[config.use_env_variable], config);
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+  sequelize = new Sequelize(process.env.DATABASE_URL, config);
 } else {
   sequelize = new Sequelize(
     config.database,
